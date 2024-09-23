bf00da9cf5464e5a5a44ef25a7a18a30b7d02d97
steph4nn <laurostephan@gmail.com>
2024-09-23 17:05:00 -0300

    wip: criação de telas com swing

--- src/appswing/TelaPrincipal.java
+++ src/appswing/TelaPrincipal.java
@@ -1,15 +1,30 @@
 package appswing;
 
 import java.awt.EventQueue;
+import java.awt.Font;
+import java.awt.Image;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
 
+import javax.swing.ImageIcon;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JMenu;
+import javax.swing.JMenuBar;
 import javax.swing.JPanel;
+import javax.swing.SwingConstants;
 import javax.swing.border.EmptyBorder;
 
 public class TelaPrincipal extends JFrame {
 
 	private static final long serialVersionUID = 1L;
 	private JPanel contentPane;
+	private JFrame frame;
+	private JMenu mnCorrentista;
+	private JMenu mnConta;
+	private JMenu mnCaixa;
+	private JLabel label;
+
 
 	/**
 	 * Launch the application.
@@ -29,14 +44,58 @@ public class TelaPrincipal extends JFrame {
 
 	/**
 	 * Create the frame.
-	 */
-	public TelaPrincipal() {
-		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 450, 300);
-		contentPane = new JPanel();
-		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+	 */private void initialize() {
+		frame = new JFrame();
+		frame.setTitle("Sistema Banko");
+		frame.setBounds(100, 100, 450, 363);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.getContentPane().setLayout(null);
 
-		setContentPane(contentPane);
+		label = new JLabel("");
+		label.setFont(new Font("Tahoma", Font.PLAIN, 26));
+		label.setHorizontalAlignment(SwingConstants.CENTER);
+		label.setText("Inicializando...");
+		label.setBounds(0, 0, 450, 313);
+//			ImageIcon imagem = new ImageIcon(getClass().getResource("/arquivos/imagem.jpg"));
+//			imagem = new ImageIcon(imagem.getImage().getScaledInstance(label.getWidth(),label.getHeight(), Image.SCALE_DEFAULT));//		label.setIcon(fotos);
+//			label.setIcon(imagem);
+		frame.getContentPane().add(label);
+		frame.setResizable(false);
+
+		JMenuBar menuBar = new JMenuBar();
+		frame.setJMenuBar(menuBar);
+		mnCorrentista = new JMenu("Correntista");
+		mnCorrentista.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseClicked(MouseEvent e) {
+				TelaCorrentista tela = new TelaCorrentista();
+			}
+		});
+		menuBar.add(mnCorrentista);
+
+		mnConta = new JMenu("Conta");
+		mnConta.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseClicked(MouseEvent e) {
+				TelaConta tela = new TelaConta();
+			}
+		});
+		menuBar.add(mnConta);
+
+		mnCaixa = new JMenu("Caixa");
+		mnCaixa.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseClicked(MouseEvent e) {
+				TelaCaixa tela = new TelaCaixa();
+			}
+		});
+		menuBar.add(mnConta);
 	}
+	public TelaPrincipal() {
+		initialize();
+		frame.setVisible(true);
+	}
+
+
 
 }
